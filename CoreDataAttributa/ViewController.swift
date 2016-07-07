623d0b59285d72e857617f394b475161e2a6907a
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-07-06 20:20:35 -0500

    Test

--- CoreDataAttributa/ViewController.swift
+++ CoreDataAttributa/ViewController.swift
@@ -9,7 +9,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var testLabel: UILabel!
     @IBOutlet weak var testTextField1: UITextField!
-    @IBOutlet weak var testTextField2: UITextField!
+    
+    let dataList = ["Integer 16","Integer 32","Integer 64","Decimal","Double","Float","String","Boolean","Date", "Binary Data", "Transformable"]
+
+    var selectStr:String = "Integer 16"
     
     //管理オブジェクトコンテキスト
     var managedContext:NSManagedObjectContext!
@@ -22,83 +25,98 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //管理オブジェクトコンテキストを取得する。
         let applicationDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         managedContext = applicationDelegate.managedObjectContext
-        //managedContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
-        
-        //保存データを表示する。
-        displayData()
         
         //デリゲート先に自分を設定する。
         testTextField1.delegate = self
-        testTextField2.delegate = self
+    }
+    
+
+    //コンポーネントの個数を返すメソッド
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
     
     
-    
-    //保存データ表示メソッド
-    func displayData(){
-        do {
-            //Playエンティティで保存されているデータを取得する。
-            let fetchRequest = NSFetchRequest(entityName: "Player")
-            let result = try managedContext.executeFetchRequest(fetchRequest) as! [Player]
-            
-            //取得したデータをラベルに表示する。
-            var outputStr = ""
-            for data in result {
-                if let name = data.name, age = data.age {
-                    outputStr = outputStr + "," + name + "," + String(age)
-                }
-            }
-            testLabel.text = outputStr
-            
-        } catch {
-            print(error)
-        }
+    //コンポーネントに含まれるデータの個数を返すメソッド
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
     }
     
+    
+    //データを返すメソッド
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataList[row]
+    }
+    
+    
+    //データ選択時の呼び出しメソッド
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        //選択されたデータを取得する。
+        selectStr = self.pickerView(pickerView, titleForRow: pickerView.selectedRowInComponent(0), forComponent: 0)!
+        
+    }
     
     
     //Returnキー押下時の呼び出しメソッド
     func textFieldShouldReturn(textField:UITextField) -> Bool {
-        
         //キーボードをしまう
         self.view.endEditing(true)
-        
         return true
     }
     
     
-    
     //ボタン押下時の呼び出しメソッド
     @IBAction func pushButton(sender: UIButton) {
-        
         do {
-            //nameの値が同じオブジェクトを検索する。
-            let fetchRequest = NSFetchRequest(entityName: "Player")
-            fetchRequest.predicate = NSPredicate(format: "name = %@", testTextField1.text!)
-            let players = try managedContext.executeFetchRequest(fetchRequest) as! [Player]
+            //新しいオブジェクトを管理オブジェクトコンテキストに格納する。
+            let player = NSEntityDescription.insertNewObjectForEntityForName("Player", inManagedObjectContext: managedContext) as! Player
+        
+            switch selectStr {
+            case "Integer 16":
+                player.testInteger16 = Int(testTextField1.text!)
+            case "Integer 32":
+                player.testInteger32 = Int(testTextField1.text!)
+            case "Integer 64":
+                player.testInteger64 = Int(testTextField1.text!)
+            case "Decimal":
+                player.testDecimal = NSDecimalNumber(double: Double(testTextField1.text!)!)
+            case "Double":
+                player.testDouble = Double(testTextField1.text!)
+            case "Float":
+                player.testFloat = Float(testTextField1.text!)
+            case "String":
+                player.testString = String(testTextField1.text!)
+            case "Boolean":
+                player.testBoolean = NSString(string:testTextField1.text!).boolValue
+            case "Date":
+                let formatter = NSDateFormatter()
+                formatter.locale = NSLocale(localeIdentifier: "ja")
+                formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+                player.testDate = formatter.dateFromString(String(testTextField1.text!))
+            case "Binary":
+                let data = NSData(data: String(testTextField1.text!).dataUsingEncoding(NSUTF8StringEncoding)!)
+                player.testBynaryData = data
+            case "Transformable":
+                player.testTransformable = String(testTextField1.text!)
             
-            
-            if (players.count == 0) {
-                //検索にヒットしなかった場合は新しいオブジェクトを管理オブジェクトコンテキストに格納する。
-                let player = NSEntityDescription.insertNewObjectForEntityForName("Player", inManagedObjectContext: managedContext) as! Player
-                player.name = testTextField1.text!
-                player.age = Int(testTextField2.text!)
                 
-            } else if(players.count == 1) {
-                //検索にヒットした場合は、そのオブジェクトを更新する。
-                players.first!.age = Int(testTextField2.text!)
-            } else {
-                print("想定外")
+            default:
+                print("とりあえずそれ以外")
             }
-            
+        
             //管理オブジェクトコンテキストに格納したデータを保存する。
             try managedContext.save()
             
-            //データを表示する。
-            displayData()
+            //Playエンティティで保存されているデータを取得する。
+            let fetchRequest = NSFetchRequest(entityName: "Player")
+            let result = try managedContext.executeFetchRequest(fetchRequest) as! [Player]
+            
+            print(result)
             
         } catch {
             print(error)
         }
     }
+
 }
--- Player+CoreDataProperties.swift
+++ Player+CoreDataProperties.swift
@@ -7,8 +7,16 @@ import CoreData
 
 extension Player {
 
-    @NSManaged var name: String?
-    @NSManaged var age: NSNumber?
-    @NSManaged var adult: NSNumber?
+    @NSManaged var testInteger16: NSNumber?
+    @NSManaged var testInteger32: NSNumber?
+    @NSManaged var testInteger64: NSNumber?
+    @NSManaged var testDecimal: NSDecimalNumber?
+    @NSManaged var testDouble: NSNumber?
+    @NSManaged var testFloat: NSNumber?
+    @NSManaged var testString: String?
+    @NSManaged var testBoolean: NSNumber?
+    @NSManaged var testDate: NSDate?
+    @NSManaged var testBynaryData: NSData?
+    @NSManaged var testTransformable: NSObject?
 
 }
